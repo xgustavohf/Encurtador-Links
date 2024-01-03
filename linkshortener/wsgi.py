0d432204943b41acd69432a746bd7051bde31cf7
Gustavo Ferreira <gustavohferreira@live.com>
2024-01-03 17:45:55 -0300

    Update static/settings

--- linkshortener/wsgi.py
+++ linkshortener/wsgi.py
@@ -15,5 +15,5 @@ from whitenoise import WhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "linkshortener.settings")
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root='/linkshortener/shortener/static/admin')
+application = WhiteNoise(application, root='/shortener/static/admin/')
 
